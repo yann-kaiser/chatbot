d64862afdb4ea8574337b0609c8c65e81b2cc728
Yann Kaiser <yann.kaiser@epita.fr>
2017-07-28 02:12:38 +0200

    [API] Fixed api calls

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let moment = require('moment')
 let Wit = require('node-wit').Wit
 let express = require('express')
 let bodyParser = require('body-parser')
-let request = require('request')
+let api = require('./api')
 
 let app = express()
 
@@ -23,9 +23,6 @@ const actions = {
   send (request, response) {
     /* const { sessionId, context, entities } = request
     const { text, quickreplies } = response */
-    console.log(request)
-    console.log(response)
-    console.log(response.text)
     messages.push({ sender: 'robot', text: response.text })
   },
   getTime ({ context, entities }) {
@@ -34,12 +31,13 @@ const actions = {
   },
   getWeather ({ context, entities }) {
     return new Promise(function (resolve, reject) {
-      let loc = entities.location ? entities.location.value : 'Paris'
+      let loc = entities.location[0] ? entities.location[0].value : 'Paris'
 
-      return callWeatherAPI(loc, (weather) => {
+      return api.callWeatherAPICallback(loc, (weather) => {
         context.weather = weather
-        if (entities.location) {
-          context.location = entities.location.value
+
+        if (entities.location[0] !== undefined) {
+          context.location = entities.location[0].value
         }
         return resolve(context)
       })
@@ -47,37 +45,7 @@ const actions = {
   }
 }
 
-// @Flow
-let callWeatherAPI = (location /* : string */, callback /* : (weather: string) */) => {
-  location = encodeURI(location)
-
-  var URL = 'http://api.openweathermap.org/data/2.5/weather?'
-  URL += 'q=' + location
-  URL += '&APPID=30248b03d4b8067fcbee8cf67702affd'
-
-  request(URL, (error, response, body) => {
-    if (error) {
-      return
-    }
-    body = JSON.parse(body)
-    var wth = body.weather[0].main.toLowerCase()
-
-    return callback(getWeatherDesc(wth))
-  })
-}
-
 const client = new Wit({ accessToken, actions })
-
-const weatherMap = {
-  'rain': 'raining',
-  'clouds': 'cloudy',
-  'sunny': 'sunny',
-  'haze': 'hazy'
-}
-
-let getWeatherDesc = (wth) => {
-  return weatherMap[wth] ? weatherMap[wth] : wth
-}
 
 app.get('/', function (req, res) {
   res.render('index', { messages: messages })
